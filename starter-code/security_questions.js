c9108f7017205b8a897b46335d2c52b623497fa8
aapiane09 <aapiane09@gmail.com>
2016-11-29 17:41:38 -0800

    Finished Security Questions

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,23 @@
 console.log("security_questions.js loaded");
+var answer;
+var securityQuestions = [
+  object1 = {
+    question : "What is your name?", expectedAnswer : "My name is Sir Arthur of Camelot."
+  },
+  object2 = {
+    question : "What is your quest?", expectedAnswer : "To seek the holy grail."
+  },
+  object3 = {
+    question : "What is the airspeed velocity of an unladen swallow?", expectedAnswer : "African or European Swallow?"
+  }];
+
+for (var i = 0; i < securityQuestions.length; i++) {
+  answer = prompt(securityQuestions[i].question);
+  if (answer != securityQuestions[i].expectedAnswer) {
+    alert("Begone, knave!");
+    var failedStatement = true;
+    break;
+  }
+}
+if (failedStatement != true) {
+alert("You may pass.")}
