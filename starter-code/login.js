ba3d703e80debed0631045aba1c0da36fa461d58
aapiane09 <aapiane09@gmail.com>
2016-11-29 18:34:33 -0800

    Finished login

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,16 @@
 console.log("login.js loaded");
+var password;
+var userLogin = {username : "b.obama", password : "goddammitjoe"};
+var loggedIn = false;
+
+while (loggedIn === false) {
+password = prompt("Enter password for user " + userLogin.username + ".");
+if (userLogin.password === password) {
+  alert("Welcome back, Mr. President.")
+  loggedIn = true;
+}
+else {
+  alert("Incorrect password!");
+  break;
+  }
+}
